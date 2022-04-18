28da02d3239e7b05c329d9889150f96d56674a11
rokas <rokasra@gmail.com>
2022-04-18 15:13:02 +0300

    asc, desc

--- src/api/usersRoutes.js
+++ src/api/usersRoutes.js
@@ -4,12 +4,19 @@ const { dbClient } = require('../config');
 const userRoutes = express.Router();
 
 // ROUTES
-userRoutes.get('/pets', async (req, res) => {
+userRoutes.get('/pets/:order?', async (req, res) => {
   try {
     // prisijungti
     await dbClient.connect();
     console.log('connected');
-    res.json('get pets route');
+    const petsArr = await dbClient
+      .db('node8')
+      .collection('pets')
+      .find()
+      .sort({ age: req.params.order?.toLowerCase() === 'dsc' ? -1 : 1 })
+      .toArray();
+    console.log('connected');
+    res.json(petsArr);
     // atlikti veiksma
   } catch (error) {
     console.error('error in get pets', error);
@@ -17,6 +24,28 @@ userRoutes.get('/pets', async (req, res) => {
   } finally {
     // uzdaryti prisijungima
     await dbClient.close();
+  }
+});
+
+userRoutes.post('/pets', async (req, res) => {
+  if (!req.body.name || !req.body.age || !req.body.type) {
+    return res.status(400).send({ err: 'Incorrect data passed' });
+  }
+
+  try {
+    // prisijungti
+    const con = await dbClient.connect();
+    console.log('connected');
+    const petsArr = await con.db('node8').collection('pets').insertOne({
+      name: req.body.name,
+      type: req.body.type,
+      age: req.body.age,
+    });
+    await con.close();
+    return res.send(petsArr);
+  } catch (error) {
+    console.error('error in get pets', error);
+    return res.status(500).send(error);
   }
 });
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const userRoutes = require('./api/usersRoutes');
 const { PORT } = require('./config');
 
@@ -7,6 +8,8 @@ const app = express();
 
 // Global MiddleWare
 app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => res.json('OKAY'));
 
